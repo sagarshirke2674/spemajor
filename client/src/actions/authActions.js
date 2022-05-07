37c797df35b7e08f94e46708a2f287052667b0a4
sagarshirke2674 <sagarshirke2674@gmail.com>
2022-05-07 12:28:28 +0530

    Update authActions.js

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -10,7 +10,7 @@ import {
 // Register User
 export const registerUser = (userData, history) => dispatch => {
     axios
-        .post("/api/users/register", userData)
+        .post(`${API}/users/register`, userData)
         .then(res => history.push("/login")) // re-direct to login on successful register
         .catch(err =>
             dispatch({
@@ -22,7 +22,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
     axios
-        .post("/api/users/login", userData)
+        .post(`${API}/users/login`, userData)
         .then(res => {
             // Save to localStorage
             // Set token to localStorage
